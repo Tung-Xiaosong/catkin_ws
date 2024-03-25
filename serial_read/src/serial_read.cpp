a59aaba1c225ebe8d40b30b4dd6db888302047fd
Tung-Xiaosong <1270964478@qq.com>
2024-03-25 19:44:11 +0800

    24.3.25

--- serial_read/src/serial_read.cpp
+++ serial_read/src/serial_read.cpp
@@ -12,14 +12,13 @@ using namespace mn::CppLinuxSerial;
 //加上校验码
 int main() 
 {
-			SerialPort serialPort("/dev/ttyUSB0", BaudRate::B_115200, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE, HardwareFlowControl::OFF, SoftwareFlowControl::OFF);
+		SerialPort serialPort("/dev/ttyUSB0", BaudRate::B_115200, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE, HardwareFlowControl::OFF, SoftwareFlowControl::OFF);
    		serialPort.SetTimeout(200); // Block when reading for 1000ms
 
-			serialPort.Open();
+		serialPort.Open();
 
     	std::this_thread::sleep_for(100ms);
-  
-    	
+   	
     	std::cout << "RobotPosition size :" << sizeof(RobotPosition) << std::endl;
     	
     	uint8_t data_buf[256];
@@ -30,48 +29,48 @@ int main()
 
     	while (true) {
     	
-    	  	std::vector<uint8_t> readData;
+    		std::vector<uint8_t> readData;
             
-          serialPort.ReadBinary(readData);
-    	
-          uint8_t *buf = readData.data();
-          
-          size_t buf_size = readData.size();
-          
-          if(buf_size > 0)
-          {
-          	memcpy(p + data_size,  buf, buf_size);
-          	data_size += buf_size;
-          }
-          
-          while( data_size >= sizeof(RobotPosition) )
-          {
-          	RobotPosition* data_ptr = reinterpret_cast<RobotPosition*>(p);
-          	
-          	if(data_ptr->head_1 == 0x55 && data_ptr->head_2 == 0xaa)
-          	{
-          		std::cout << "Received Position X: " << data_ptr->position_x << std::endl;
-            	std::cout << "Received Position Y: " << data_ptr->position_y << std::endl;
-            	std::cout << "Received Position Yaw: " << data_ptr->position_yaw << std::endl;
-            	
-            	data_size -= sizeof(RobotPosition);
-            	p += sizeof(RobotPosition);
-          	}
-          	else
-          	{
-          		-- data_size;
-          		++ p;
-          	}
-          	
-          }
-          
-          if(data_size > 0 && p != data_buf)
-          {
-          	memmove(&data_buf[0], p, data_size);
-          }
-          
-          p = &data_buf[0];
-          
+			serialPort.ReadBinary(readData);// 从串口serialPort中读取二进制数据到readData容器中
+			
+			uint8_t *buf = readData.data();
+			
+			size_t buf_size = readData.size();
+			
+			if(buf_size > 0)// 读取到了数据
+			{
+				memcpy(p + data_size,  buf, buf_size);
+				data_size += buf_size;
+			}
+			
+			while( data_size >= sizeof(RobotPosition) )
+			{
+				RobotPosition* data_ptr = reinterpret_cast<RobotPosition*>(p);
+				
+				if(data_ptr->head_1 == 0x55 && data_ptr->head_2 == 0xaa)
+				{
+					std::cout << "Received Position X: " << data_ptr->position_x << std::endl;
+					std::cout << "Received Position Y: " << data_ptr->position_y << std::endl;
+					std::cout << "Received Position Yaw: " << data_ptr->position_yaw << std::endl;
+					
+					data_size -= sizeof(RobotPosition);
+					p += sizeof(RobotPosition);
+				}
+				else
+				{
+					-- data_size;
+					++ p;
+				}
+				
+			}
+			
+			if(data_size > 0 && p != data_buf)
+			{
+				memmove(&data_buf[0], p, data_size);
+			}
+			
+			p = &data_buf[0];
+            
 		}
     serialPort.Close();
 
